1472e94b989a944d5338346864d0d1b7ada2b632
moriz-code <morzavi1@gmail.com>
2020-05-04 05:03:17 +0300

    ipload

--- frontend/src/views/ToyApp.js
+++ frontend/src/views/ToyApp.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import { loadItems, deleteItem } from '../actions/toyActions'
 import ToyList from '../cmps/ToyList'
-import ToyFilter from '../cmps/ToyFilter';
+
 
 
 
